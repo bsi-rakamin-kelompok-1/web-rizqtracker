f6d7cd0668a6a5fb7a2dc91a2ba4ac8ae27f392b
aldinmldn <aldinmldn@gmail.com>
2025-04-29 19:04:28 +0700

    add table component

--- src/components/cashflow/ExpenseCard.tsx
+++ src/components/cashflow/ExpenseCard.tsx
@@ -79,23 +79,23 @@ const ExpenseCard = () => {
   return (
     <div className="p-4">
       <div className="flex justify-center mb-6">
-  <div className="flex rounded-md overflow-hidden border border-gray-300 bg-white">
-    {filters.map((filter, index) => (
-      <Button
-        key={filter}
-        className={`px-4 py-2 text-sm font-medium ${
-          selectedFilter === filter
-            ? "bg-yellow-400 text-black"
-            : "bg-white text-black"
-        } ${index !== filters.length ? "hover:bg-yellow-500 w-30" : ""}`}
-        onClick={() => setSelectedFilter(filter)}
-        variant="ghost"
-      >
-        {filter}
-      </Button>
-    ))}
-  </div>
-</div>
+        <div className="flex rounded-md overflow-hidden border border-gray-300 bg-white">
+          {filters.map((filter, index) => (
+            <Button
+              key={filter}
+              className={`px-4 py-2 text-sm font-medium ${
+                selectedFilter === filter
+                  ? "bg-yellow-400 text-black"
+                  : "bg-white text-black"
+              } ${index !== filters.length ? "hover:bg-yellow-500 w-30" : ""}`}
+              onClick={() => setSelectedFilter(filter)}
+              variant="ghost"
+            >
+              {filter}
+            </Button>
+          ))}
+        </div>
+      </div>
 
       <div className="space-y-4">
         {filteredTransactions.map((tx) => (
@@ -106,13 +106,15 @@ const ExpenseCard = () => {
             <CardContent className="p-1 flex flex-col sm:flex-row justify-between w-full items-center">
               <div>
                 <p className="font-medium text-[#EF4444]">
-                  {
-                    transactionType === "needs" ? "Kebutuhan"
-                    : transactionType === "bills" ? "Pembayaran"
-                    : transactionType === "shopping" ? "Belanja"
-                    : transactionType === "transport" ? "Transportasi"
-                    : "Kesehatan"
-                  }
+                  {transactionType === "needs"
+                    ? "Kebutuhan"
+                    : transactionType === "bills"
+                    ? "Pembayaran"
+                    : transactionType === "shopping"
+                    ? "Belanja"
+                    : transactionType === "transport"
+                    ? "Transportasi"
+                    : "Kesehatan"}
                 </p>
               </div>
               <div>
